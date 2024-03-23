b1fca3247a23836618296daf79a29274e8045b76
USMPDNBJ <david_nolasco@usmp.pe>
2024-03-23 10:34:05 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TechStore_PL1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
