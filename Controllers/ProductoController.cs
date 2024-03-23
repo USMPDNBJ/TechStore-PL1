f433802dc236663defa97ccdbeb3932385eadd72
USMPDNBJ <david_nolasco@usmp.pe>
2024-03-23 10:35:15 -0500

    "Formulario"

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mymovieapp.Models;
+
+namespace mymovieapp.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar([Bind("Nombre,Descripcion,Precio")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Message"] = "IGV del Producto: "+producto.Precio*0.18;
+                return View("Index");
+            }
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
